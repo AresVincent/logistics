973343699eb9ee9966730aaec5dc644390c813ba
AresVincent <vincentso@hkares.com>
2021-10-05 11:18:25 +0800

    milestone

--- admin/__JS__/demo.js
+++ admin/__JS__/demo.js
@@ -356,8 +356,8 @@ $(function () {
 function getBaseUrl(){
     // return 'http://192.168.100.104:8777/'; 
     // return 'http://192.168.100.103:8083/';
-    // return 'http://8.210.123.128:8888/';
-      return 'http://122.10.80.32:8888/'; 
+    return 'http://8.210.123.128:8888/';
+    //   return 'http://122.10.80.32:8888/'; 
 }
 function getLeftMenu(menuName){
     var menu=["area","charging","outlets","user","order"]
